5fa1f4275a41e3c19f19fdaf5e607e8945ad80fc
Julia Herron <julia.herron@locusenergy.com>
2016-10-05 19:26:14 -0700

    puppies

--- alembic/env.py
+++ alembic/env.py
@@ -17,8 +17,8 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from demo.models import Cupcakes
-target_metadata = Cupcakes.metadata
+from demo.models import Puppies
+target_metadata = Puppies.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- demo/models.py
+++ demo/models.py
@@ -2,15 +2,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 db = SQLAlchemy(app)
 
 
-class Cupcakes(db.Model):
-    __tablename__ = 'cupcakes'
+class Puppies(db.Model):
+    __tablename__ = 'puppies'
     id = db.Column(db.Integer, primary_key=True)
-    delicious = db.Column(db.Boolean)
-    quantity = db.Column(db.Integer)
+    name = db.Column(db.String(80))
+    cuteness = db.Column(db.Integer)
+    birthday = db.Column(db.DateTime)
+
 
 
 
